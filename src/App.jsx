9ab8601352ef89438a6f43c57da0da12e7e67645
yeiversonyj <yeivercrack@gmail.com>
2023-11-08 19:16:27 -0500

    :fire: Remove non-functional private routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,49 +10,15 @@ import RecoverPassword from './Pages/RecoverPassword';
 import Register from './Pages/Register';
 
 function App() {
-  // Verificar si el usuario está autenticado
-  const isAuthenticated = () => {
-    return localStorage.getItem('isUserAuthenticated') === 'true';
-  };
-
-  // Componente de ruta protegida
-  const ProtectedRoute = ({ element }) => {
-    if (!isAuthenticated()) {
-      return <Navigate to="/login" replace />;
-    }
-
-    return element;
-  };
-
-  // Lógica de inicio de sesión
-  const handleLogin = () => {
-    // Procesar el inicio de sesión
-    // Por ejemplo, después de verificar las credenciales, establecer el estado de autenticación en verdadero
-    localStorage.setItem('isUserAuthenticated', 'true');
-    <Route path="/dashboard" element={<NavbarPage />} />;
-  };
-
-  // Lógica de cierre de sesión
-  const handleLogout = () => {
-    // Procesar el cierre de sesión
-    // Por ejemplo, después de cerrar sesión, eliminar el estado de autenticación
-    localStorage.removeItem('isUserAuthenticated');
-  };
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<AcercaNosotros />} />
       <Route path="/contact" element={<Contact />} />
-      <Route path="/login" element={<Login onLogin={handleLogin} />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgotpassword" element={<RecoverPassword />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute element={<NavbarPage onLogout={handleLogout} />} />
-        }
-      />
+      <Route path="/dashboard" element={<NavbarPage />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
